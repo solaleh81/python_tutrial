0ebf1dd169d01eaa1adcee6b66934f836b930a2b
solaleh81 <solalehalizadeh81.com>
2023-05-07 12:14:04 +0430

    sd

--- solale.py
+++ solale.py
@@ -16,6 +16,14 @@ def increment(number, by, another):
 
 
 print(increment(number=2, by=1, another=20))
+
+# multiplication table
 for x in range(1, 10):
     for y in range(1, 10):
-        print(x * y) + "\t"
+        print(x * y, end="\t\t")
+    print("\n")
+
+
+def save_user(**user):
+    print(user)
+    save_user(id=1, name="ali", age=32)
